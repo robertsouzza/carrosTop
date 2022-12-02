73ec210875ad033ca70567f5a55a711123ac2e41
robertsouzza <rcsouzza@gmail.com>
2022-12-02 18:12:21 -0400

    catalog page

---
+++ frontweb/src/pages/Catalog/index.tsx
@@ -0,0 +1,33 @@
+import ProductCard from '../../components/ProductCard'
+import SearchButton from '../../components/SearchButton'
+
+const Catalog = () => {
+  return (
+
+    <div className="container my-4">
+      <SearchButton />
+      <div className="row">
+        <div className="col-sm-6 col-lg-4 col-xl-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-4">
+          <ProductCard />
+        </div>
+        <div className="col-sm-6 col-lg-4 col-xl-4">
+          <ProductCard />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Catalog;
